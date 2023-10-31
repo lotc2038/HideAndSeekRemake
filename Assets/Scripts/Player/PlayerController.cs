b9592519dbb0e0259e17bfa514d4a8270d4dfd9a
Сергей Пилипенко <lotc2038@gmail.com>
2023-11-01 05:06:23 +0700

    Player movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    float mouseSensitivity, moveSpeed, jumpForce;
+    float mouseSensitivity, moveSpeed, jumpForce, rotateSpeed;
 
+    public Transform cameraTransform;
 
     Rigidbody rb;
 
@@ -17,6 +18,24 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        transform.Rotate(Vector3.up * Input.GetAxisRaw("Mouse X") * mouseSensitivity);
+       // transform.Rotate(Vector3.up * Input.GetAxisRaw("Mouse X") * mouseSensitivity);
+
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 cameraForward = Vector3.Scale(cameraTransform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 moveDirection = (verticalInput * cameraForward + horizontalInput * cameraTransform.right).normalized;
+
+        // ѕоворачиваем персонаж в соответствии с направлением движени€
+        if (moveDirection != Vector3.zero)
+        {
+            Quaternion newRotation = Quaternion.LookRotation(moveDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
+        }
+
+        // ѕеремещаем персонаж
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime, Space.World);
+
     }
 }
