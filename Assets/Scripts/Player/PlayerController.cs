8da883c9a7074ec2abc5b8e8e1338d740d67cb26
Сергей Пилипенко <lotc2038@gmail.com>
2023-11-02 03:59:25 +0700

    Added player movement and camera

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,35 +8,68 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     float mouseSensitivity, moveSpeed, jumpForce, rotateSpeed;
 
-    public Transform cameraTransform;
+    float verticalLookRotation;
+    
+    //TODO: Сделать передачу никнейма и команды игрока
+   
+
+    public GameObject cameraHolder;
 
     Rigidbody rb;
+    PhotonView pv;
+
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        pv = GetComponent<PhotonView>();
+
     }
 
+    private void Start()
+    {
+        if (!pv.IsMine)
+        {
+            Destroy(GetComponentInChildren<Camera>().gameObject);
+        }
+    }
+
+    //TODO: Вынести управление в отдельный метод?
     void Update()
     {
-       // transform.Rotate(Vector3.up * Input.GetAxisRaw("Mouse X") * mouseSensitivity);
+      if(!pv.IsMine) return;
 
+        Look();
+        Move();
+       
+    }
 
+    void Look()
+    {
+        transform.Rotate(Vector3.up * Input.GetAxisRaw("Mouse X") * mouseSensitivity);
+
+        verticalLookRotation += Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90f, 90f);
+
+        cameraHolder.transform.localEulerAngles = Vector3.left * verticalLookRotation;
+    }
+
+    void Move()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        Vector3 cameraForward = Vector3.Scale(cameraTransform.forward, new Vector3(1, 0, 1)).normalized;
-        Vector3 moveDirection = (verticalInput * cameraForward + horizontalInput * cameraTransform.right).normalized;
-
-        // Поворачиваем персонаж в соответствии с направлением движения
-        if (moveDirection != Vector3.zero)
-        {
-            Quaternion newRotation = Quaternion.LookRotation(moveDirection);
-            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
-        }
+        Vector3 cameraForward = Vector3.Scale(cameraHolder.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 moveDirection = (verticalInput * cameraForward + horizontalInput * cameraHolder.transform.right).normalized;
 
         // Перемещаем персонаж
         transform.Translate(moveDirection * moveSpeed * Time.deltaTime, Space.World);
-
     }
+
+
+ 
+
+
+
+
 }
